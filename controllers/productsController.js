dd0637821b30e6c75ab96cc979d7684c6348f478
santaforse <santaforsell@gmail.com>
2022-12-13 22:37:07 +0200

    adding files to the new repository

---
+++ controllers/productsController.js
@@ -0,0 +1,11 @@
+let products = require('../data/database')
+const express = require('express')
+const controller = express.Router()
+
+
+controller.route('/').get((req, res) => {
+    res.status(200).json(products)
+})
+
+
+module.exports = controller 
---
+++ controllers/usersController.js
@@ -0,0 +1,7 @@
+let users = require('../data/database')
+const express = require('express')
+const controller = express.Router()
+
+
+
+module.exports = controller 
